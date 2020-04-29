39618474cf7fc1712dc44129fca23141cebfb001
Cees <cjol99@icloud.com>
2020-04-29 23:47:03 +0200

    Fucking google

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,6 +5,7 @@ class Head extends React.Component {
   render () {
     return (
       <Helmet>
+				<meta name="google-site-verification" content="GruPFYXMwtkpol49D8EfOkCyl5wyoS2x9D4w-yPRFxY" />
         <meta
           name="description"
           content="Some description"
