d091914e0bf1a036ac6fc9cbd5005e203713b045
Cees <cjol99@icloud.com>
2020-04-30 13:58:37 +0200

    Add dev indication

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export const Header = ({ headerStyle, isAuthenticated, startLogout }) => (
 		<div className="content-container">
 			<div className="header__content">
 				<Link className="header__title nostyle" to="/">
-					Cees Jol
+					Cees Jol {process.env.NODE_ENV === 'development' && ' - Dev'}
 				</Link>
 				{isAuthenticated && (
 					<button className="button button--link" onClick={startLogout}>Logout</button>
