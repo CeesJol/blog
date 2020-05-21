8a8524d23f81df972e8e6c70041c1ebae78ae118
Cees <cjol99@icloud.com>
2020-05-21 19:26:49 +0200

    Add sitemap

---
+++ sitemap-builder.js
@@ -0,0 +1,19 @@
+const { sitemapBuilder } = require('react-router-sitemap');
+const path = require('path'); // add path which will be needed for file write
+const fs = require('fs'); // import file system object
+
+const routes = ['/'];
+
+// use your website root address here. Optimally you can
+// include dev and production enviorenments with variable
+const hostname = 'http://localhost:3000';
+
+// define our destination folder and sitemap file name
+const dest = path.resolve('./public', 'sitemap.xml');
+
+// Generate sitemap and return Sitemap instance
+const sitemap = sitemapBuilder(hostname, routes);
+
+// write sitemap.xml file in /public folder
+// Access the sitemap content by converting it with .toString() method
+fs.writeFileSync(dest, sitemap.toString())
